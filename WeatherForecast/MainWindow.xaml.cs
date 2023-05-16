b08036f30da61c00d7c7a6f02a783ddb9a080e8e
Артем <artemzaripov2002@gmail.com>
2023-05-16 04:38:19 +0300

    Добавьте файлы проекта.

---
+++ WeatherForecast/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Windows;
+
+namespace WeatherForecast
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Метод, активирующийся при нажатии на кнопку
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string city = City.Text;
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&appid=ffc05b61c96f4a360f8244f609f2ad83";
+
+            try
+            {
+                HttpClient httpClient = new();
+
+                ModuleData? moduleData = await httpClient.GetFromJsonAsync<ModuleData>(url);
+
+                double? celsius = (moduleData?.Main?.Temp) - 273.15;
+
+                Temperature.Text = Math.Round((decimal)celsius, 2).ToString();
+                Wind.Text = moduleData?.Wind?.Speed.ToString() + " м/с";
+                Description.Text = moduleData?.Weather?[0].Description;
+            }
+            catch
+            {
+                Error.Text = "Город не найден";
+            }
+        }
+    }
+}
---
+++ WeatherForecast/Weather.cs
@@ -0,0 +1,23 @@
+﻿namespace WeatherForecast;
+
+public class ModuleData
+{
+    public Weather[]? Weather { get; set; }
+    public Main? Main { get; set; }
+    public Wind? Wind { get; set; }
+}
+
+public class Weather
+{
+    public string? Description { get; set; }
+}
+
+public class Main
+{
+    public double Temp { get; set; }
+}
+
+public class Wind
+{
+    public double Speed { get; set; }
+}
